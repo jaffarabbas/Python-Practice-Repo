b3ba13f8960582b4ec812ad7ccb264e1ff4d1340
jaffarabbas <gamaportal8@gmail.com>
2021-04-24 18:41:13 +0500

    4-24-2021

---
+++ Aptech_Work/OOp/classpr2.py
@@ -0,0 +1,10 @@
+def f1():
+    print("sdfsdf")
+
+def main():
+    print("dsfdsf")
+
+if __name__ == '__main__':
+    f1()
+    main()
+    
---
+++ Random/rand.py
@@ -0,0 +1,48 @@
+#guess the number game
+import random
+
+class game:
+     count = 0
+     mincount = 5
+     score = 10
+     finalscore = 0
+     
+     def genrateNumber(self,start,end):
+         return random.randint(start,end)
+    
+     def Guesser(self):
+        while True:
+            if(self.count != 3):
+                while True:
+                    print("GUSESS NUMBER")
+                    number = int(input('Guess Number : '))
+                    
+                    ran = self.genrateNumber(0,10)
+                    
+                    if(number == ran):
+                        print(ran," is the guess number won !!!" , self.mincount , self.count)
+                        self.finalscore += self.score
+                        break
+                    else:
+                        self.mincount -= 1
+                        if self.mincount == -1:
+                            print("Faild ")
+                            break
+                        else:
+                            print("Wrong !! count left : " , self.mincount , "Current level : ",self.count) 
+                                
+                self.count += 1           
+            else:
+                print("Score : ",self.finalscore)
+                break
+
+def main():
+    ob = game()
+    # ran =  ob.genrateNumber(0,10)
+    # print(ran)
+    ob.Guesser()
+
+
+if __name__ == '__main__':
+    main()
+    
