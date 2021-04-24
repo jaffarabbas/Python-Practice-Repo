c7f52176b309a0932e44736f58dc523c5fe38ccf
jaffarabbas <gamaportal8@gmail.com>
2021-04-24 09:03:38 +0500

    4-24-2021

---
+++ WebScrapper/pdfdownloader.py
@@ -0,0 +1,9 @@
+import urllib.request
+pdf_path = ""
+def download_file(download_url, filename):
+    response = urllib.request.urlopen(download_url)    
+    file = open(filename + ".pdf", 'wb')
+    file.write(response.read())
+    file.close()
+ 
+download_file(pdf_path, "Test")
