a06ff8b6d7edaebc3a9c48643aa41b43bd37d031
jaffarabbas <gamaportal8@gmail.com>
2021-05-07 19:45:57 +0500

    7-may-2021      GUI work in Tkinter

---
+++ TkinterPractice/labelwindow.py
@@ -0,0 +1,9 @@
+import tkinter
+
+rout = tkinter.Tk()
+
+text1 = tkinter.Label(rout,text="Hello jaffar it your first gui in python",)
+
+text1.pack()
+
+rout.mainloop()
---
+++ TkinterPractice/main.py
@@ -0,0 +1,4 @@
+import tkinter
+
+top = tkinter.Tk()
+top.mainloop()
