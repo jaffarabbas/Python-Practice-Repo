3aeda8073ed92770c98c15a4f7cd7b0ea8d079db
jaffarabbas <gamaportal8@gmail.com>
2021-06-24 20:31:45 +0500

    6-24-2021

---
+++ Aptech_Work/dataconnection/dataApp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.CharField(max_length=60)
+    phone = models.CharField(max_length=12)
+    message = models.TextField(max)
+    date = models.DateField()
