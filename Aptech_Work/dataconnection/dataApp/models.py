c172fee433d8e7f8c931579bcc61c5979c131174
jaffarabbas <gamaportal8@gmail.com>
2021-06-29 14:17:14 +0500

    6-29-2021

--- Aptech_Work/dataconnection/dataApp/models.py
+++ Aptech_Work/dataconnection/dataApp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Contact(models.Model):
@@ -8,3 +9,6 @@ class Contact(models.Model):
     phone = models.CharField(max_length=12)
     message = models.TextField(max)
     date = models.DateField()
+
+    def __str__(self):
+        return self.name
--- Aptech_Work/dataconnection/dataApp/views.py
+++ Aptech_Work/dataconnection/dataApp/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, HttpResponse
-
+from _datetime import datetime
+from . import models
 
 # Create your views here.
+from .models import Contact
+
 
 def index(request):
-    return render(request, "index.html")
+    obj = Contact.objects.all()
+
+    context = {
+        'object': obj
+    }
+    return render(request, "index.html", context)
 
 
 def about(request):
@@ -12,4 +20,18 @@ def about(request):
 
 
 def contact(request):
-    return render(request, "Contact.html")
+    flag = False
+    if request.method == "POST":
+        name1 = request.POST.get('name')
+        email1 = request.POST.get('email')
+        phone1 = request.POST.get('phone')
+        message1 = request.POST.get('message')
+        date1 = datetime.now()
+        modeldata = Contact(name=name1, email=email1, phone=phone1, message=message1, date=date1)
+        modeldata.save()
+        flag = True
+
+    context = {
+        'flag': flag
+    }
+    return render(request, "Contact.html", context)
