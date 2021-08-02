d1bd435f9dc29bd679a78d6896c20099097c3c1f
jaffarabbas <gamaportal8@gmail.com>
2021-08-02 22:39:11 +0500

    8-2-2021

---
+++ Project_Project/test.py
@@ -0,0 +1,22 @@
+# functions
+# 1-taking user info
+# 2-generate random pass
+# 3-shuffle input in it
+
+class GeneratePassword:
+    __a = 23
+    
+    def __init__(self):
+        print(self)
+
+    def UserInfo(self):
+        print(self.__a)
+
+
+def main():
+    gpobj = GeneratePassword()
+    print(gpobj)
+
+
+if __name__ == '__main__':
+    main()
