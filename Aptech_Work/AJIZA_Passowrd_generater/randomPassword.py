8978b334697f045deabd40d39b203d4efcd4d292
jaffarabbas <gamaportal8@gmail.com>
2021-08-13 19:43:55 +0500

    8-13-2021

--- Aptech_Work/AJIZA_Passowrd_generater/randomPassword.py
+++ Aptech_Work/AJIZA_Passowrd_generater/randomPassword.py
@@ -1,10 +1,14 @@
 from tkinter import *
 import random, string
+from copyPassword import CopyPassword
 
 
+# Check box class for checkbox group
 class CheckBar(Frame):
-    def __init__(self, parent=None, picks=[], side=LEFT, anchor=W):
+    def __init__(self, parent=None, picks=None, side=LEFT, anchor=W):
         Frame.__init__(self, parent)
+        if picks is None:
+            picks = []
         self.vars = []
         for pick in picks:
             var = IntVar()
@@ -18,21 +22,6 @@ class CheckBar(Frame):
 
 class RandomPassword:
     password = ''
-
-    def GeneratePassword(self, CheckList):
-        SpecialCharacters = "@$-&.%!_#"
-        if CheckList[0] == 1:
-            self.password += ''.join(random.choice(string.ascii_lowercase) for i in range(3))
-        if CheckList[1] == 1:
-            self.password += ''.join(random.choice(string.ascii_uppercase) for i in range(3))
-        if CheckList[2] == 1:
-            self.password += ''.join(random.choice(string.digits) for i in range(3))
-        if CheckList[3] == 1:
-            self.password += ''.join(random.choice(string.ascii_letters) for i in range(3))
-        if CheckList[4] == 1:
-            self.password += ''.join(random.choice(SpecialCharacters) for i in range(3))
-
-        return ''.join(random.choice(self.password) for i in range(15))
 
     def main_window(self):
         randomPasswordWindow = Tk()
@@ -52,13 +41,51 @@ class RandomPassword:
         lng = CheckBar(randomPasswordWindow, ['Lowercase', 'Uppercase', 'Numbers', 'Default', 'special characters'])
         lng.pack(side=TOP, fill=X)
 
-        Output.insert(END, list(lng.state()))
         Output.pack()
 
+        def Copy():
+            CopyPassword.CopyPassword(Output.get("1.0",END))
+
+        Button(randomPasswordWindow, text='Copy', command=Copy).pack()
+
         def checkCheckBox():
+            Output.configure(state="normal")
+            self.password = ''
             Output.delete('1.0', END)
             Output.insert(END, self.GeneratePassword(list(lng.state())))
+            Output.configure(state='disabled')
 
         Button(randomPasswordWindow, text='Peek', command=checkCheckBox).pack(side=RIGHT)
 
         randomPasswordWindow.mainloop()
+
+    def GeneratePassword(self, CheckList):
+        SpecialCharacters = "@$-&.%!_#"
+        try:
+            if CheckList[0] == 1:
+                self.password += ''.join(random.choice(string.ascii_lowercase) for i in range(3))
+            if CheckList[1] == 1:
+                self.password += ''.join(random.choice(string.ascii_uppercase) for i in range(3))
+            if CheckList[2] == 1:
+                self.password += ''.join(random.choice(string.digits) for i in range(3))
+            if CheckList[3] == 1:
+                self.password += self.GenerateDefaultPassword()
+            if CheckList[4] == 1:
+                self.password += ''.join(random.choice(SpecialCharacters) for i in range(3))
+            if CheckList.count(0) == 5:
+                print("Error")
+        except EXCEPTION:
+            print(EXCEPTION)
+
+        return ''.join(random.choice(self.password) for i in range(15))
+
+    # generate random password by default
+    def GenerateDefaultPassword(self):
+        SpecialCharacters = "@$-&.%!_#"
+        randomLowerString = ''.join(random.choice(string.ascii_lowercase) for i in range(4))
+        randomUpperString = ''.join(random.choice(string.ascii_uppercase) for i in range(4))
+        randomDigit = ''.join(random.choice(string.digits) for i in range(4))
+        randomSpecialString = ''.join(random.choice(SpecialCharacters) for i in range(3))
+        tempPassword = randomLowerString + randomUpperString + randomSpecialString + randomDigit
+        return ''.join(random.choice(tempPassword) for i in range(len(tempPassword)))
+
