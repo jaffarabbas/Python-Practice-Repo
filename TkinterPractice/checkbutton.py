7b3167849bcbf39d713170155dbed87c199422a6
jaffarabbas <gamaportal8@gmail.com>
2021-08-16 20:12:28 +0500

    8-16-2021

--- TkinterPractice/checkbutton.py
+++ TkinterPractice/checkbutton.py
@@ -1,30 +1,12 @@
-from tkinter import *
-
-top = Tk()
-top.geometry("500x500")
-CheckBox_1 = Checkbutton(top, variable=varValueForCheckBox, onvalue=1, offvalue=0, height=0,
-                                 width=0, bg="white",
-                                 image=checkBeforeImage,
-                                 selectimage=checkAfterImage, indicatoron=False, borderwidth=0, ).place(x=260, y=350)
-
-        CheckBox_2 = Checkbutton(, variable=varValueForCheckBox2, onvalue=1, offvalue=0, height=0,
-                                 width=0, bg="white",
-                                 image=checkBeforeImage,
-                                 selectimage=checkAfterImage, indicatoron=False, borderwidth=0, ).place(x=565, y=350)
-        CheckBox_3 = Checkbutton(, variable=varValueForCheckBox, onvalue=1, offvalue=0, height=0,
-                                 width=0, bg="white",
-                                 image=checkBeforeImage,
-                                 selectimage=checkAfterImage, indicatoron=False, borderwidth=0, ).place(x=838, y=350)
-        CheckBox_4 = Checkbutton(, variable=varValueForCheckBox, onvalue=1, offvalue=0, height=0,
-                                 width=0, bg="white",
-                                 image=checkBeforeImage,
-                                 selectimage=checkAfterImage, indicatoron=False, borderwidth=0, ).place(x=220, y=395)
-        CheckBox_5 = Checkbutton(, variable=varValueForCheckBox, onvalue=1, offvalue=0, height=0,
-                                 width=0, bg="white",
-                                 image=checkBeforeImage,
-                                 selectimage=checkAfterImage, indicatoron=False, borderwidth=0, ).place(x=630, y=395)
-        self.checkValue.append(varValueForCheckBox2)
-
-top.mainloop()
-
-
+# Check box class for checkbox group
+# class CheckBar(Frame):
+#     def __init__(self, parent=None, picks=None, side=LEFT, anchor=W):
+#         Frame.__init__(self, parent)
+#         if picks is None:
+#             picks = []
+#         self.vars = []
+#         for pick in picks:
+#             var = IntVar()
+#             chk = Checkbutton(self, text=pick, variable=var)
+#             chk.pack(side=side, anchor=anchor, expand=YES)
+#             self.vars.append(var)
