a74ac6e7639223d877ecae855b3f7894f9ba5cf3
jaffarabbas <fa19bsse0008@maju.edu.pk>
2021-09-25 14:06:12 +0500

    25/09/2021 14:06:08

---
+++ GitPython/main.py
@@ -0,0 +1,18 @@
+# from github import Github
+# from git import Git
+# import os
+# from dotenv import load_dotenv
+#
+# load_dotenv()
+#
+#
+# def main():
+#     # credentials = Github(os.getenv("USERNAME"), os.getenv("PASSWORD"))
+#     # repos = credentials.search_repositories(query="language:python")
+#     # for i in credentials.get_user().get_repos():
+#     #     print(i.name)
+#     p = Git.
+#
+#
+# if __name__ == '__main__':
+#     main()
