8ee3fed6e537a8c85336ba908cc9676727a00d90
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-22 21:05:42 +0500

    Updated selenium-integration/opportunity/opportunityTestCases.py at 22/08/2022 21:05:41

--- Selenium Practice/selenium-integration/opportunity/opportunityTestCases.py
+++ Selenium Practice/selenium-integration/opportunity/opportunityTestCases.py
@@ -1,8 +1,29 @@
+import time
 import unittest
+from selenium.webdriver.common.by import By
+from lead.leadPom import LeadPom
+from opportunity.opportunityPom import OpportunityPom
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
 
-if __name__ == '__main__':
-    unittest.main()
+def opportunitySuite():
+    suite = unittest.TestSuite()
+    suite.addTest(TestOpportunity('test_positive_opportunity_creation'))
+    return suite
+
+
+class TestOpportunity(unittest.TestCase):
+
+    estimateClose = "01/01/2020"
+    slider = "100"
+    value = 4000
+    textField = "Test Lead"
+    timeInsert = "Monthly"
+    contact = "Saravanan"
+
+    _checkMessage = "Task__description"
+    _message = textField
+
+    opportunityCreation = OpportunityPom()
+
+    def test_positive_opportunity_creation(self):
+        self.opportunityCreation.createOpportunity(self.estimateClose, self.slider, self.value,self.timeInsert, self.contact,self.textField)
