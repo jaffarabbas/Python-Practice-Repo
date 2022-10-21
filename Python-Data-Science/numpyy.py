b8fc4c7a987dba6fe532abbfe8804ee50543ed34
Jaffar Abbas <gamaportal8@gmail.com>
2022-10-21 23:00:40 +0500

    Commited File : numpyy.py at 10/21/2022 11:00:40 PM

---
+++ Python-Data-Science/numpyy.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+# print(np.array([1,-1])*np.array([1,1]))
+# print(np.dot(np.array([1,-1]),np.array([1,1])))
+
+A = np.array([[1,2],[3,4],[5,6],[7,8]])
+
+B = np.array([[1,2,3],[4,5,6],[7,8,9]])
+
+# print(np.dot(A,B))
+
+a=np.array([0,1,0,1,0])
+
+b=np.array([1,0,1,0,1])
+
+print(a*b)
+# print(a*b)
+
+a=np.array([0,1])
+
+b=np.array([1,0])
+
+# print(np.dot(a,b))
+
+a=np.array([1,1,1,1,1])
+
+# print(a+10)
