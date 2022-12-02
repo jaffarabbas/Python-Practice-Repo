9f5cee50181a6881c5a94c47353e5cd032882444
jaffarabbas <gamaportal8@gmail.com>
2022-12-02 13:41:33 +0500

    Inserted tempCodeRunnerFile.py at 02/12/2022 13:41:33

---
+++ HackerRank/tempCodeRunnerFile.py
@@ -0,0 +1,19 @@
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    queries = []
+
+    for _ in range(m):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = arrayManipulation(n, queries)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
