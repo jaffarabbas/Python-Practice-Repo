e2518990c2fe006d2f21aafdc0cb13235dd98e05
jaffarabbas <gamaportal8@gmail.com>
2024-03-19 19:42:53 +0500

    setup modal middleware

--- SentimentAnalysis/SentimentAnalysisModels/main.py
+++ SentimentAnalysis/SentimentAnalysisModels/main.py
@@ -1,2 +1,34 @@
-if __name__ == '__main__':
-    print("asdasd")
+from pycorenlp import StanfordCoreNLP
+import json
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+from textblob import TextBlob
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from flair.models import TextClassifier
+from flair.data import Sentence
+from transformers import pipeline
+import pandas as pd
+
+
+def falir_model(text):
+    classifier = TextClassifier.load('en-sentiment')
+    sentence = Sentence(text)
+    classifier.predict(sentence)
+    return sentence.labels[0].value
+
+
+def text_blob(text):
+    blob = TextBlob(text)
+    sentiment = 0.0
+    for sentence in blob.sentences:
+        sentiment += sentence.sentiment.polarity
+    return sentiment
+
+
+def evaluatePolartiy(plority, range):
+    if plority < range[0]:
+        return "Negative"
+    elif plority == range[1]:
+        return "Neutral"
+    else:
+        return "Positive"
--- SentimentAnalysis/nltk/main.py
+++ SentimentAnalysis/nltk/main.py
@@ -1,41 +1,19 @@
-# import nltk
-# from nltk.sentiment import SentimentIntensityAnalyzer
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
 
-# # Download the VADER lexicon if not already downloaded
-# nltk.download('vader_lexicon')
-
-# # Create a SentimentIntensityAnalyzer object
-# sia = SentimentIntensityAnalyzer()
-
-# # Sample text for sentiment analysis
-# text = "i am frustuated with the product. It's not working as expected."
-
-# # Analyze sentiment
-# sentiment_scores = sia.polarity_scores(text)
-
-# # Get the compound sentiment score
-# compound_score = sentiment_scores['compound']
-
-# # Determine sentiment label based on the compound score
-# if compound_score >= 0.05:
-#     sentiment_label = 'positive'
-# elif compound_score <= -0.05:
-#     sentiment_label = 'negative'
-# else:
-#     sentiment_label = 'neutral'
-
-# # Print the predicted sentiment label
-# print("Predicted Sentiment:", sentiment_label)
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
+# Download the VADER lexicon if not already downloaded
+nltk.download('vader_lexicon')
 
 # Create a SentimentIntensityAnalyzer object
 sia = SentimentIntensityAnalyzer()
 
 # Sample text for sentiment analysis
-text = "Kindly resolve this on priority bases as this issue is stop our daily working."
+text = "i am frustuated with the product. It's not working as expected."
 
 # Analyze sentiment
 sentiment_scores = sia.polarity_scores(text)
+
+print(sentiment_scores)
 
 # Get the compound sentiment score
 compound_score = sentiment_scores['compound']
